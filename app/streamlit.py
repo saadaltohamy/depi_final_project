99d588a79819771254dce82f06ef9443ee6a1c44
Saad Altohamy <Saad.dodo444@gmail.com>
2024-10-16 22:57:32 +0300

    streamlit init

---
+++ app/streamlit.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from model_function import summarize_text  # Import your summarization function
+
+st.title("Summarization App")
+
+# Create a text input for users
+input_text = st.text_area("Enter text to summarize:")
+
+if st.button("Summarize"):
+    if input_text:
+        # Directly use the summarize_text function
+        summary = summarize_text(input_text)
+        st.write("Summary:")
+        st.write(summary)
+    else:
+        st.warning("Please enter some text to summarize.")
