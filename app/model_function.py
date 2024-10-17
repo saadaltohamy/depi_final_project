49488a9abaa84e393ecf0c253559233f637c84cb
AhmedShetaia <117360204+AhmedShetaia@users.noreply.github.com>
2024-10-17 11:25:20 +0300

    Update model_function.py
    get only the summarized text not the whole dictionary

--- app/model_function.py
+++ app/model_function.py
@@ -4,6 +4,6 @@ pipe = pipeline("summarization", model="facebook/bart-large-cnn")
 
 def summarize_text(sentence):
 
-    summary = summarizer(sentence, max_length=130, min_length=30, do_sample=False)
+    summary = summarizer(sentence, max_length=130, min_length=30, do_sample=False)[0]['summary_text']
 
     return summary
