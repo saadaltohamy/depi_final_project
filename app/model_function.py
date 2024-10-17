a6750bbe431f08137991cb0829266cc10a777b60
AhmedShetaia <117360204+AhmedShetaia@users.noreply.github.com>
2024-10-17 12:03:37 +0300

    Update model_function.py

--- app/model_function.py
+++ app/model_function.py
@@ -4,6 +4,6 @@ summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
 
 def summarize_text(sentence):
 
-    summary = summarizer(sentence, max_length=130, min_length=30, do_sample=False)[0]['summary_text']
+    summary = summarizer(sentence, max_length=250, min_length=150, do_sample=False)[0]['summary_text']
 
     return summary
