d4cefbcebeddf447eb05b475278a56d9d2f39aac
AhmedShetaia <117360204+AhmedShetaia@users.noreply.github.com>
2024-10-23 12:58:21 +0300

    changed the name to SummaBot

--- app/streamlit.py
+++ app/streamlit.py
@@ -1,29 +1,16 @@
 import streamlit as st
 from model_function import summarize_text  # Import your summarization function
 
-st.title("Summarization App")
+st.title("SummaBot")
 
+# Create a text input for users
 input_text = st.text_area("Enter text to summarize:")
-
-# Define the character limit for the preview
-PREVIEW_LIMIT = 300  # Customize as needed
 
 if st.button("Summarize"):
     if input_text:
-        # Generate summary
+        # Directly use the summarize_text function
         summary = summarize_text(input_text)
-        
-        # Display a preview if the summary is long
-        if len(summary) > PREVIEW_LIMIT:
-            st.write("Summary (Preview):")
-            st.write(summary[:PREVIEW_LIMIT] + "...")
-            
-            # Use an expander for the full summary text
-            with st.expander("Read more"):
-                st.write(summary)
-        else:
-            # Display the full summary if it's short
-            st.write("Summary:")
-            st.write(summary)
+        st.write("Summary:")
+        st.write(summary)
     else:
         st.warning("Please enter some text to summarize.")
